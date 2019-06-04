109e38cbf878f14232d7eca6d3cd2f1b3a1b6678
cmulliss <cmulliss@gmail.com>
2019-06-04 18:28:35 +0100

    pokedex started

---
+++ pokedex/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './App.css'
+import Pokedex from './Pokedex'
+
+function App () {
+  return (
+    <div className='App'>
+      <Pokedex />
+    </div>
+  )
+}
+
+export default App
+
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/';
+// const POKE_API = 'https://assets.pokemon.com/assets/cms2/img/pokedex/detail/'
+
+// for each pokemon, their image src would be:
+// const POKE_API = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png';
+// https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png
+
+// {id: 4, name: 'Charmander', type: 'fire', base_experience: 62}
